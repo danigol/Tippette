c074b6316361b93162b2f1aea85fe0bc0472cb62
Danielle Golinsky <degolinsky@icloud.com>
2019-07-13 15:10:02 -0400

    Initial Commit.

---
+++ app/src/main/java/com/daniellegolinsky/tippette/PayUpActivity.kt
@@ -0,0 +1,12 @@
+package com.daniellegolinsky.tippette
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PayUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay_up)
+    }
+}
