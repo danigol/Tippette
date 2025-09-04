1c9a02ad2bdf2e9a83e9295adddf0602183e5601
Danielle Golinsky <degolinsky@icloud.com>
2025-09-04 00:14:46 -0400

    Updated versions and page view model.

---
+++ app/src/main/java/com/daniellegolinsky/tippette/PageViewModel.kt
@@ -0,0 +1,8 @@
+package com.daniellegolinsky.tippette
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class PageViewModel @Inject constructor(val tipModel: TipModel) : ViewModel() {
+
+}
